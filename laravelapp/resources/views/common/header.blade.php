3b74bd404d123ef128df9492c715d06881ffc622
stranger <>
2021-05-09 10:10:19 +0900

    #2 ヘッダー部分作成(細かい調整は未実施)

--- laravelapp/resources/views/common/header.blade.php
+++ laravelapp/resources/views/common/header.blade.php
@@ -1,20 +1,33 @@
 @section('header')
-    <header>
-    <ul>
-        <li class="nav-item ml-2">
-            Anti-Cheat
-        </li>
-        <li>
-            検索
-        </li>
-        <li>
-            <a class="nav-link text-white">ログイン</a>
-        
-        </li>
-        <li>
-            <a class="nav-link text-white">サインアップ</a>
-        
-        </li>
-    </ul>
-    </header>
+        <header>
+            <nav class="navbar navbar-expand-lg flex-row navbar-dark bg-dark mt-3">
+                <a class="navbar-brand mr-auto" href="#">AntiCheat</a>
+                
+                <form class="form-inline my-2 my-lg-0">
+                    <div class="mr-4">
+                        <input type="radio" name="search_figure" value="movie" checked="checked"><a class="navbar-text">動画</a>
+                        <input type="radio" name="search_figure" value="tag"><a class="navbar-text">タグ</a>
+                    </div>
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">動画検索</button>
+                
+                    <ul class="navbar-nav ml-4">
+                        <li　class="nav-item">
+                            <a class="navbar-text">検索</a>
+                        </li>
+                      
+                        <li　class="nav-item">
+                            <a class="navbar-text">ログイン</a>
+                        </li>
+                        <li　class="nav-item">
+                            <a class="navbar-text">サインアップ</a>
+                        </li>
+                    </ul>
+                    </form>
+                </div>
+            </nav>
+        </header>
+
+
+
 @show
--- laravelapp/resources/views/layouts/app.blade.php
+++ laravelapp/resources/views/layouts/app.blade.php
@@ -1,6 +1,7 @@
 <html>
 <head>
     <title>@yield('title')</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 
 <body>
