f0db1440a8246cb8962054c3ea0fce7b6ff7ddb6
stranger <>
2021-06-22 19:31:18 +0900

    #4 テーブル作成

---
+++ laravelapp/database/migrations/2021_06_21_103832_create_mov_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMovTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mov', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('mov_name');
+            $table->string('mov_file_dir');
+            $table->date('upload_by');
+            $table->integer('number_view');
+            $table->integer('bookmarks');
+            $table->integer('updated_by')->unsigned();
+            $table->foreign('updated_by')->references('id')->on('user')->onDelete('no action');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mov');
+    }
+}
