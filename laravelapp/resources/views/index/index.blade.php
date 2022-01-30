9c4eafcfb17e86d61181207af1ec322c68b6e473
CCraida <crowcry7@yahoo.co.jp>
2022-01-30 11:06:30 +0900

    #22 下部コンテンツ作成

--- laravelapp/resources/views/index/index.blade.php
+++ laravelapp/resources/views/index/index.blade.php
@@ -10,22 +10,21 @@
     </div>
 -->
 
-<div class="container">
+<div class="container-fluid m-4">
     <?php $i = 0 ?>
 
+    <form method="post">
     <!-- 1行に三つのサムネを表示する-->
     @foreach($mov_info as $mov_record)
 
-        @if (($i == 0) or ($i % 3 == 0)  )
+        @if (($i == 0) or ($i % 4 == 0)  )
             <div class="row">
         @endif
-        <div>
-            <form method="post">
+        <div class="mr-4 mb-4">
                 <!--type="submit"のinputタグとボタンの画像となるimg タグを用意-->
                <a href="/play_movie/{{ $mov_record->id }}"><img src="{{$mov_record->thumb_dir}}" alt="サムネイル" class="img-fluid" width="300" height= "300"  >
                 </a>
-                <p class="text-center">ここにタイトルです</p>
-            </form>
+                <p class="text-center">{{ $mov_record['mov_name'] }}</p>
 <!--
             <a href="test">
                 <img src="{{$mov_record->thumb_dir}}" alt="サムネイル" class="img-fluid" width="300" height= "300"  >
@@ -36,11 +35,12 @@
         </div>
         <?php $i++?>
         <!-- 一行の限界三つ目の表示の場合、もしくは最後のサムネの場合。 -->
-        @if((($i % 3 == 0) and( $i != 0 ) ) or ( $loop->last))
+        @if((($i % 4 == 0) and( $i != 0 ) ) or ( $loop->last))
             </div>
         @endif
 
     @endforeach
+    </form>
 </div>
 @endsection
 
